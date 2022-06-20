67fdb5d2a7e14c2e55303738e1319adac9305ef3
boccosantiago <italobocco@gmail.com>
2022-06-20 12:41:29 +0200

    Ejercicio 4

--- sprint6/src/App.js
+++ sprint6/src/App.js
@@ -1,14 +1,35 @@
-import React from 'react'
-import Escena from './components/escena/Escena';
-
-
+import React from "react";
+import Escena from "./components/escena/Escena";
+import { ButtonsDiv, MainDiv } from "./components/escena/styled";
+import { useState } from "react";
+import { Nav } from "./components/escena/Nav";
+import frases from "./components/escena/Frases";
 
 function App() {
+  
+  const [count, setCount] = useState(1);
+
+  function handleClick(i) {
+    setCount(prevCount => {
+      if (prevCount + i > frases.length || prevCount + i <= 0)
+        return prevCount;
+      return prevCount + i;
+    })
+  }
+  
+  console.log(count)
+ 
+
   return (
-     <div>
-    <Escena/>
-    </div>
-  )
+    <MainDiv>
+    <ButtonsDiv>
+      <Nav handleClick={handleClick}/> 
+    </ButtonsDiv>
+     <Escena numEscena={count}/>
+    </MainDiv>
+  );
+
+ 
 }
 
 export default App;
